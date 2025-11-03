50f0b3ef779d0b4d985ef0ceaeb6030cef5cb9c9
Anjali Sharma <anjalidecodes@gmail.com>
2025-11-03 23:40:19 +0530

    @staticmethod

---
+++ methods/static.py
@@ -0,0 +1,15 @@
+class Calculator:
+    @staticmethod #decorator
+    def add(a,b):
+        return a + b
+    
+    @staticmethod
+    def multiply(a,b):
+        return a * b
+
+#Calling static methods on the class itself rather than the instance of the class.
+#self parameter is not required
+
+result1 = Calculator.add(10, 20)
+result2 = Calculator.multiply(5, 8)
+print(result1, result2)
