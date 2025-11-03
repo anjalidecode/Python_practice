9a8d482310cf52912b920f2a66179ddfc91b9a91
Anjali Sharma <anjalidecodes@gmail.com>
2025-11-04 00:27:26 +0530

    method chaining with operators

---
+++ methods/chaining.py
@@ -0,0 +1,26 @@
+class Calculator:
+    def __init__(self, value=0):
+        self.value = value
+
+    def add(self, num):
+        self.value += num
+        return self
+    
+    def subtract(self, num):
+        self.value -= num
+        return self
+    
+    def multiply(self, num):
+        self.value *= num
+        return self
+    
+    def result(self):
+        return self.value
+
+    
+my_calculator = Calculator()
+
+final_result = my_calculator.add(5).subtract(2).multiply(3).result()
+print(f"The result of the chained operation is {final_result}")
+
+        
