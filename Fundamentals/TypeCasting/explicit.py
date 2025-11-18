478367337279edb3e0389c1fa0719e9f220478ac
Anjali Sharma <anjalidecodes@gmail.com>
2025-11-18 21:54:35 +0530

    Type Conversion

---
+++ Fundamentals/TypeCasting/explicit.py
@@ -0,0 +1,13 @@
+# Done manually by programmer
+num_int = 123
+num_str = "456"
+
+# converting num_str from string to int
+num_str = int(num_str)
+
+print("Data type of num_str after Type casting :", type(num_str))
+
+num_sum = num_int + num_str
+
+print("Sum:", num_sum)
+print("Data type:", type(num_sum))
--- Fundamentals/TypeCasting/implicit.py
+++ Fundamentals/TypeCasting/implicit.py
@@ -5,3 +5,4 @@ num_flt = 1.23
 new_num = num_int + num_flt
 
 print("Value of new_num :" ,new_num)
+print("Data type of new_num :", type(new_num ))
