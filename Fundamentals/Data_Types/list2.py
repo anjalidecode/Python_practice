03171a5e9d39ba3a45394977a88e7086957988f8
Anjali Sharma <anjalidecodes@gmail.com>
2025-11-18 22:05:23 +0530

    list manipulation

---
+++ Fundamentals/Data_Types/list2.py
@@ -0,0 +1,17 @@
+# Creating a list
+my_list = ["apple", "banana", "cherry", "orange"]
+
+# Accesing elements in the list
+print(my_list[0])
+print(my_list[-1])
+
+# Adding and removing elements
+my_list.append("grape")
+print(my_list)
+
+my_list.remove("banana")
+print(my_list)
+
+# Slicing a list
+sublist = my_list[1:3]
+print(sublist)
